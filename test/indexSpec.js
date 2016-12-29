ce05aabdd33e78a1c8d83120d89a1f115dbc4384
DJ Adams <dj.adams@pobox.com>
2016-12-29 14:56:17 +0000

    initial commit

---
+++ test/indexSpec.js
@@ -0,0 +1,31 @@
+const R = require('ramda');
+
+const teams = [
+	{ name : "Lions", score : 5 },
+	{ name : "Tigers", score : 4 },
+	{ name : "Bears", score : 6 },
+	{ name : "Monkeys", score : 2 }
+];
+
+const totalScore = R.pipe(
+	R.pluck("score"),
+	R.reduce(R.add, 0)
+);
+
+const winningTeam = R.pipe(
+	R.sortBy(R.prop("score")),
+	R.last(),
+	R.prop("name")
+);
+
+describe("Football team results", () => {
+
+	it("should calculate the total score", () => {
+		totalScore(teams).should.eql(17);
+	});
+
+	it("should identify the winning team", () => {
+		winningTeam(teams).should.eql("Bears");
+	});
+
+});
